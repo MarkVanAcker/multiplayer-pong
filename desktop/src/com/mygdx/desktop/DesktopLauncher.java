eb46b1454bcb2ab850c713b2bb05b990a6c7b43a
s0153120 <markvanacker@student.uantwerpen.be>
2017-09-18 22:06:50 +0200

    client added

--- desktop/src/com/mygdx/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/desktop/DesktopLauncher.java
@@ -2,7 +2,7 @@ package com.mygdx.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.Pong;
+
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
