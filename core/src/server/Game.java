c71f5799067947602f4b13158d6b04e36ab633dc
Hypsilon <floris.vermeulen@hotmail.com>
2017-09-18 22:09:58 +0200

    game class constructor

--- core/src/server/Game.java
+++ core/src/server/Game.java
@@ -1,12 +1,14 @@
 package server;
 
+import com.esotericsoftware.kryonet.Server;
+
 public class Game implements Runnable {
 
     private World world;
     private ServerProgram serverProgram;
 
-    public Game() {
-
+    public Game(ServerProgram serverProgram) {
+        this.serverProgram = serverProgram;
     }
 
     @Override
